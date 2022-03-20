6f2713363d3cf2a07f29646ed4d1c02b4327bb14
jiangy <jiangyi15@mails.ucas.ac.cn>
2022-03-20 13:19:14 +0800

    add readme in benchmark

--- benchmark/fit.py
+++ benchmark/fit.py
@@ -5,7 +5,7 @@ config = ConfigLoader("config.yml")
 config.set_params("gen_params.json")
 
 with tf.device("GPU"):
-    fit_result = config.fit() # without_grad=True)
+    fit_result = config.fit() # jac="2-points")
 fit_result.set_error(config.get_params_error())
 fit_result.save_as("final_params.json")
 
